602b07ac49f7cb7775a0618292cb877c7affe6c8
Pedro Verruma <pedrovam@gmail.com>
2011-05-05 00:29:43 +0100

    v0.1

---
+++ README.md
@@ -0,0 +1,19 @@
+# procvm
+procvm is a linux shell scripts that list VM size, PID and comand for each process.
+
+## Usage
+`./procvm.sh`
+
+## Example
+`1       23 MB           init (/sbin/init)
+1009    5 MB            getty (/sbin/getty -8 38400 tty2)
+10128   216 MB          apache2 (/usr/sbin/apache2 -k start)
+1013    5 MB            getty (/sbin/getty -8 38400 tty6)
+10154   216 MB          apache2 (/usr/sbin/apache2 -k start)
+1016    33 MB           avahi-daemon (avahi-daemon: chroot helper)
+10169   206 MB          apache2 (/usr/sbin/apache2 -k start)
+1019    3 MB            acpid (acpid -c /etc/acpi/events -s /var/run/acpid.socket)`
+
+## Future Features
+* Filter by VM size.
+* Units: KB, MB, GB, ...
---
+++ procvm.sh
@@ -0,0 +1,32 @@
+﻿#!/bin/bash
+
+# procvm.sh (v0.1)
+# https://github.com/pmav/procvm
+#
+# http://pmav.eu
+
+for process in `ls -m /proc/ | cut --delimiter=',' --fields=1- | grep '^[0-9]' | tr -d ','`; do
+
+       if [ -f /proc/$process/status ]; then
+
+               vmsize=`cat /proc/$process/status | grep VmSize`
+               
+               if [ `expr length "$vmsize"` -gt 0 ]; then
+
+			           name=`cat /proc/$process/status | grep Name`
+                       name=${name/Name:}
+                       name=`echo $name | tr -s " "`
+               
+                       vmsize=${vmsize/VmSize:}
+                       vmsize=${vmsize/kB}
+                       vmsize=`echo $vmsize | tr -s " "`
+                       vmsize=`echo "scale=0; $vmsize / 1024" | bc`
+
+                       processpath=`ps -p $process -o args --no-heading | nawk '{print substr($0,1,120)}' | awk '{gsub(/^ +| +$/,"")}1'`
+
+                       echo -e "$process\t$vmsize MB\t\t`tput setaf 2`$name`tput op` ($processpath)"
+               fi
+       fi
+done
+
+tput op
