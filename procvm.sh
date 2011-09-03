a6b8250ea121d1b9da4feaa8b94269c0d6ebf5f6
Pedro Verruma <pedrovam@gmail.com>
2011-09-03 22:17:49 +0100

    VmRSS added.

--- procvm.sh
+++ procvm.sh
@@ -1,30 +1,42 @@
-# procvm.sh (v0.1)
+# procvm.sh (v0.2.0)
 #
 # https://github.com/pmav/procvm
 #
 # http://pmav.eu
 
+echo -e "PID\tVmRSS\tVmSize\t\tName (Path)\n"
+
 for process in `ls -m /proc/ | cut --delimiter=',' --fields=1- | grep '^[0-9]' | tr -d ','`; do
 
-       if [ -f /proc/$process/status ]; then
+	if [ -f /proc/$process/status ]; then
 
-               vmsize=`cat /proc/$process/status | grep VmSize`
-               
-               if [ `expr length "$vmsize"` -gt 0 ]; then
+		process_status=`cat /proc/$process/status`
 
-                       name=`cat /proc/$process/status | grep Name`
-                       name=${name/Name:}
-                       name=`echo $name | tr -s " "`
-               
-                       vmsize=${vmsize/VmSize:}
-                       vmsize=${vmsize/kB}
-                       vmsize=`echo $vmsize | tr -s " "`
-                       vmsize=`echo "scale=0; $vmsize / 1024" | bc`
+		if [[ "$process_status" =~ "VmSize:" ]]; then
 
-                       processpath=`ps -p $process -o args --no-heading | nawk '{print substr($0,1,120)}' | awk '{gsub(/^ +| +$/,"")}1'`
+			# Handle Name
+			name=$(echo "$process_status" | grep 'Name')
+			name=${name/Name:}
+			name=`echo $name | tr -s " "`
 
-                       echo -e "$process\t$vmsize MB\t\t`tput setaf 2`$name`tput op` ($processpath)"
-               fi
+			# Handle VmSize
+			vmsize=$(echo "$process_status" | grep 'VmSize')
+			vmsize=${vmsize/VmSize:}
+			vmsize=${vmsize/kB}
+			vmsize=`echo $vmsize | tr -s " "`
+			vmsize=`echo "scale=0; $vmsize / 1024" | bc`
+
+			# Handle VmRSS
+			vmrss=$(echo "$process_status" | grep 'VmRSS')
+			vmrss=${vmrss/VmRSS:}
+			vmrss=${vmrss/kB}
+			vmrss=`echo $vmrss | tr -s " "`
+			vmrss=`echo "scale=0; $vmrss / 1024" | bc`
+
+			processpath=`ps -p $process -o args --no-heading | nawk '{print substr($0,1,120)}' | awk '{gsub(/^ +| +$/,"")}1'`
+
+			echo -e "$process\t$vmrss MB\t$vmsize MB\t\t`tput setaf 2`$name`tput op` ($processpath)"
+		fi
        fi
 done
 
